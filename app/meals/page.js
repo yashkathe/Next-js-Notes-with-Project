302fefcd6040b53a6eec9bbde49f779883de9b1e
yashkathe <yashkathe123@gmail.com>
2024-01-02 17:38:22 +0000

    Update 02-original-project to output generated at 46bb156

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,26 @@
-export default function MealsPage(second) {
-    return <h1>Meals</h1>
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
+
+import { getMeals } from "@/fetch-functions/meals";
+
+export default async function MealsPage(second) {
+	const meals = await getMeals();
+
+	return (
+		<>
+			<header className={classes.header}>
+				<h1>
+					Meals created <span className={classes.highlight}>by you</span>
+				</h1>
+				<p>Choose a meal and cook it yourself</p>
+				<p className={classes.cta}>
+					<Link href='meals/share'>Share Your favourite recepie</Link>
+				</p>
+			</header>
+			<main className={classes.main}>
+				<MealsGrid meals={meals} />
+			</main>
+		</>
+	);
 }
