0cee56ee3bf274a54e65bbb3d518c69db02174bf
yashkathe <yashkathe123@gmail.com>
2024-01-03 21:32:59 +0000

    Update 02-original-project to output generated at 98f5471

--- fetch-functions/meals.js
+++ fetch-functions/meals.js
@@ -4,6 +4,6 @@ const db = sql("meals.db");
 
 // creatig extra delay just for practice
 export async function getMeals() {
-	await new Promise((resolve) => setTimeout(resolve, 2000));
+	await new Promise((resolve) => setTimeout(resolve,2000));
 	return db.prepare("SELECT * FROM meals").all();
 }
