e31a502e2906bb5078f6f9766855c26b7d55938d
Yash Kathe <yashkathe123@gmail.com>
2024-01-05 14:34:36 -0500

    not found and error pages added

---
+++ 02-Food-app-main-project/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+	return (
+		<main className='not-found'>
+			<h1>Not Found</h1>
+			<p>The Page you requested is not a Valid Route</p>
+		</main>
+	);
+}
--- 02-Food-app-main-project/fetch-functions/meals.js
+++ 02-Food-app-main-project/fetch-functions/meals.js
@@ -5,5 +5,7 @@ const db = sql("meals.db");
 // creatig extra delay just for practice
 export async function getMeals() {
 	await new Promise((resolve) => setTimeout(resolve,2000));
-	return db.prepare("SELECT * FROM meals").all();
+	
+    // throw new Error('error')
+    return db.prepare("SELECT * FROM meals").all();
 }
