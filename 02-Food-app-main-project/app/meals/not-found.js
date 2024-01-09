50e90b35491d53ae30175978f6b55ba4aad5419f
Yash Kathe <yashkathe123@gmail.com>
2024-01-09 11:52:57 -0800

    added image picker component and loading and rendering details via dynamic routes

---
+++ 02-Food-app-main-project/app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+	return (
+		<main className='not-found'>
+			<h1>Meal not Found</h1>
+			<p>The Meal Page you requested does not exist</p>
+		</main>
+	);
+}
--- 02-Food-app-main-project/fetch-functions/meals.js
+++ 02-Food-app-main-project/fetch-functions/meals.js
@@ -4,8 +4,12 @@ const db = sql("meals.db");
 
 // creatig extra delay just for practice
 export async function getMeals() {
-	await new Promise((resolve) => setTimeout(resolve,2000));
-	
-    // throw new Error('error')
-    return db.prepare("SELECT * FROM meals").all();
+	await new Promise((resolve) => setTimeout(resolve, 2000));
+
+	// throw new Error('error')
+	return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMeal(slug) {
+	return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
