b78cdf8496c5c4ad1401483ce10593267af97777
Yash Kathe <yashkathe123@gmail.com>
2024-01-09 21:22:18 -0800

    using useFormStatus to manage form submission and useFormState to display error messages on same page

---
+++ 02-Food-app-main-project/app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error({ error }) {
+	return (
+		<main className='error'>
+			<h1>Failed to Create Meal</h1>
+			<p>Invalid Inputs Entered</p>
+		</main>
+	);
+}
--- 02-Food-app-main-project/fetch-functions/actions.js
+++ 02-Food-app-main-project/fetch-functions/actions.js
@@ -3,7 +3,11 @@
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
-export async function shareMeal(formData) {
+function isInvalidText(text) {
+	return !text.title || text.trim() === "";
+}
+
+export async function shareMeal(prevState, formData) {
 	const meal = {
 		title: formData.get("title"),
 		summary: formData.get("summary"),
@@ -12,6 +16,21 @@ export async function shareMeal(formData) {
 		creator: formData.get("name"),
 		creator_email: formData.get("email"),
 	};
+
+	if (
+		isInvalidText(meal.title) ||
+		isInvalidText(meal.summary) ||
+		isInvalidText(meal.instructions) ||
+		isInvalidText(meal.creator) ||
+		isInvalidText(meal.creator_email) ||
+		!meal.creator_email.includes("@") ||
+		!meal.image ||
+		meal.image.size === 0
+	) {
+		return {
+			message: "Invalid input",
+		};
+	}
 
 	await saveMeal(meal);
 	redirect("/meals");
