10562027139457d5e2664670aa73bbd856af3123
Yash Kathe <yashkathe123@gmail.com>
2024-01-09 16:53:15 -0800

    Creating Server Actions and storing uploaded image into public folder and its path in database

--- 02-Food-app-main-project/app/meals/share/page.js
+++ 02-Food-app-main-project/app/meals/share/page.js
@@ -1,4 +1,5 @@
 import ImagePicker from "@/components/meals/image-picker";
+import { shareMeal } from "@/fetch-functions/actions";
 
 import classes from "./page.module.css";
 
@@ -12,7 +13,7 @@ export default function ShareMealPage() {
 				<p>Or any other meal you feel needs sharing!</p>
 			</header>
 			<main className={classes.main}>
-				<form className={classes.form}>
+				<form className={classes.form} action={shareMeal}>
 					<div className={classes.row}>
 						<p>
 							<label htmlFor='name'>Your name</label>
@@ -39,7 +40,7 @@ export default function ShareMealPage() {
 							rows='10'
 							required></textarea>
 					</p>
-					<ImagePicker />
+					<ImagePicker label='Your Image' name='image' />
 					<p className={classes.actions}>
 						<button type='submit'>Share Meal</button>
 					</p>
